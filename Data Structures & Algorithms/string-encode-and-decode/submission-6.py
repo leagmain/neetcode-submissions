5467873a31b2f9567da66c307cf8a340f3ec365a
leon <leagmain@gmail.com>
2026-04-25 15:26:52 -0700

    Add: string-encode-and-decode - submission-6

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-6.py
@@ -0,0 +1,15 @@
+class Solution:
+
+    def __init__(self):
+        self.SEP = chr(260)
+        self.EMPTY = chr(271)
+
+    def encode(self, strs: List[str]) -> str:
+        if len(strs) == 0:
+            return self.EMPTY
+        return self.SEP.join(strs)
+
+    def decode(self, s: str) -> List[str]:
+        if s == self.EMPTY:
+            return []
+        return s.split(self.SEP)
